8ab7ef7e774e4e0294bda35d0b8ee3f7835d732a
Lucas <lucashen@gmail.com>
2018-02-16 18:27:15 -0200

    change layout panel

--- src/app/panel/panel.component.ts
+++ src/app/panel/panel.component.ts
@@ -1,15 +1,118 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-panel',
-  templateUrl: './panel.component.html',
-  styleUrls: ['./panel.component.css']
+    selector: 'app-panel',
+    templateUrl: './panel.component.html',
+    styleUrls: ['./panel.component.css']
 })
 export class PanelComponent implements OnInit {
 
-  constructor() { }
+    orders = [];
+    constructor() { }
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+        this.orders = [{
+            id: 1,
+            products: [{
+                id: 1,
+                name: 'Coxinha',
+                quantity: 1
+            }, {
+                id: 2,
+                name: 'Coca-cola',
+                quantity: 1
+            }, {
+                id: 3,
+                name: 'Água sem gás',
+                quantity: 1
+            }],
+            table: 1,
+            status: 4
+        }, {
+            id: 2,
+            products: [{
+                id: 1,
+                name: 'Coxinha',
+                quantity: 1
+            }, {
+                id: 2,
+                name: 'Coca-cola',
+                quantity: 1
+            }, {
+                id: 3,
+                name: 'Água sem gás',
+                quantity: 1
+            }],
+            table: 2,
+            status: 3
+        }, {
+            id: 3,
+            products: [{
+                id: 1,
+                name: 'Coxinha',
+                quantity: 1
+            }, {
+                id: 2,
+                name: 'Coca-cola',
+                quantity: 1
+            }, {
+                id: 3,
+                name: 'Água sem gás',
+                quantity: 1
+            }],
+            table: 3,
+            status: 2
+        }, {
+            id: 4,
+            products: [{
+                id: 1,
+                name: 'Coxinha',
+                quantity: 1
+            }, {
+                id: 2,
+                name: 'Coca-cola',
+                quantity: 1
+            }, {
+                id: 3,
+                name: 'Água sem gás',
+                quantity: 1
+            }],
+            table: 4,
+            status: 1
+        }, {
+            id: 5,
+            products: [{
+                id: 1,
+                name: 'Coxinha',
+                quantity: 1
+            }, {
+                id: 2,
+                name: 'Coca-cola',
+                quantity: 1
+            }, {
+                id: 3,
+                name: 'Água sem gás',
+                quantity: 1
+            }],
+            table: 5,
+            status: 1
+        }, {
+            id: 6,
+            products: [{
+                id: 1,
+                name: 'Coxinha',
+                quantity: 1
+            }, {
+                id: 2,
+                name: 'Coca-cola',
+                quantity: 1
+            }, {
+                id: 3,
+                name: 'Água sem gás',
+                quantity: 1
+            }],
+            table: 6,
+            status: 1
+        }];
+    }
 }
